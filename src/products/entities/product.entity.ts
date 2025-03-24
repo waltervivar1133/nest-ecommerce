c57dc9d97dfc12fb168868cee3e0597e94b4ff1c
walter vivar <waltervivar1133@gmail.com>
2025-03-24 09:55:42 -0500

    feat: initial commit

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,51 @@
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar', {
+    length: 255,
+    unique: true,
+  })
+  title: string;
+
+  @Column('float', {
+    default: 0,
+  })
+  price: number;
+
+  @Column('text', {
+    nullable: true,
+  })
+  description: string;
+
+  @Column('varchar', {
+    length: 255,
+    unique: true,
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+
+  @Column('text', {
+    array: true,
+  })
+  sizes: string[];
+
+  @BeforeInsert()
+  checkSlugInsert() {
+    if (!this.slug) {
+      this.slug = this.title;
+    }
+
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
+}
